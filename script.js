5339c4fd4ae9b2124a9c765d6e0f8e1b706ee439
Mansimran Kaur <91445014+Mansimran24@users.noreply.github.com>
2022-06-03 20:28:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // WEB303 Assignment 2
 // Mansimran Kaur
-
 $(document).ready(function () {
     $("#prospect").on("click", function () {
       let abcd = new XMLHttpRequest();
@@ -16,4 +15,34 @@ $(document).ready(function () {
       };
       abcd.send();
     });
+    $("#convert").on("click", function () {
+        let abcd = new XMLHttpRequest();
+    
+        abcd.open("GET", "convert.html", true);
+    
+        abcd.onload = function () {
+          if (abcd.status == 200) {
+            let convrt = document.getElementById("content");
+            convrt.style.display = "block";
+            convrt.innerHTML =abcd.responseText;
+          }
+        };
+        abcd.send();
+      });
+      $("#retain").on("click", function () {
+        let abcd = new XMLHttpRequest();
+    
+        abcd.open("GET", "retain.html", true);
+    
+        abcd.onload = function () {
+          if (abcd.status == 200) {
+            let ret = document.getElementById("content");
+            ret.style.display = "block";
+            ret.innerHTML = abcd.responseText;
+          }
+        };
+        abcd.send();
+      });
+  
+    
   });

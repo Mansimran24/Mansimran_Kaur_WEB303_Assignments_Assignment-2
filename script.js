62a979812d6aed66eca3b87f09b62f5ffa9dcf54
Mansimran Kaur <91445014+Mansimran24@users.noreply.github.com>
2022-06-03 22:36:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,14 @@
 // WEB303 Assignment 2
 // Mansimran Kaur
-$(document).ready(function () {
-    $("#prospect").on("click", function () {
-      let abcd = new XMLHttpRequest();
-  
-      abcd.open("GET", "prospect.html", true);
-  
-      abcd.onload = function () {
-        if (abcd.status == 200) {
-          let Pro = document.getElementById("content");
-          Pro.style.display = "block";
-          Pro.innerHTML = abcd.responseText;
-        }
-      };
-      abcd.send();
-    });
-    $("#convert").on("click", function () {
-        let abcd = new XMLHttpRequest();
-    
-        abcd.open("GET", "convert.html", true);
-    
-        abcd.onload = function () {
-          if (abcd.status == 200) {
-            let convrt = document.getElementById("content");
-            convrt.style.display = "block";
-            convrt.innerHTML =abcd.responseText;
-          }
-        };
-        abcd.send();
-      });
-      $("#retain").on("click", function () {
-        let abcd = new XMLHttpRequest();
-    
-        abcd.open("GET", "retain.html", true);
-    
-        abcd.onload = function () {
-          if (abcd.status == 200) {
-            let ret = document.getElementById("content");
-            ret.style.display = "block";
-            ret.innerHTML = abcd.responseText;
-          }
-        };
-        abcd.send();
-      });
-  
-    
-  });
+function makeXHRCallForHTML(){ 
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "retain.html", true);
+
+    xhr.onload=fuction(){
+      if(xhr.status == 200){
+        let el = document.getElementById("content")
+        el.innerHTML= xhr.responseText;
+      }
+    };
+  }
